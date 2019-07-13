dcf65f8ec26916cdcc4104d25b67d86ecdd8c97d
yz3440 <mars_zhao@outlook.com>
2019-07-13 20:24:09 +0800

    change

--- src/main.js
+++ src/main.js
@@ -2,18 +2,23 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VuePlyr from 'vue-plyr';
+import 'vue-plyr/dist/vue-plyr.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(VuePlyr);
+Vue.use(ElementUI);
+
 
 Vue.config.productionTip = false;
 
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App),
-  created() {
-
-  },
-  methods: {
-
-  },
+  created() {},
+  methods: {},
 
 }).$mount('#app');
