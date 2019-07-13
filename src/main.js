18df2ce3364896fdc12584c6847d90dfdfe338a1
grace_g <zg650@nyu.edu>
2019-07-13 20:23:13 +0800

    saving landing page progress

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 Vue.config.productionTip = false;
 
 new Vue({
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import landing from './views/landing.vue';
 
 Vue.use(Router);
 
@@ -9,9 +10,14 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/',
+      name: 'landing',
+      component: landing,
     },
     {
       path: '/about',
